41f605d230fd2c032284a18ec57c98b5ff30a0e0
REIGaruki <gyakujo@mail.ru>
2024-02-13 10:11:59 +0300

    Initial C

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 100500;
+        byte b = 17;
+        short sh = 999;
+        long l = 123456789L;
+        float bottle = (float) 1.5;
+        double pi = 3.14159265358979323846264338327950288419716939937510;
+        System.out.println("Значение переменной "+"i"+" с типом "+"int"+" равно "+i);
+        System.out.println("Значение переменной "+"b"+" с типом "+"byte"+" равно "+b);
+        System.out.println("Значение переменной "+"sh"+" с типом "+"short"+" равно "+sh);
+        System.out.println("Значение переменной "+"l"+" с типом "+"long"+" равно "+l);
+        System.out.println("Значение переменной "+"botle"+" с типом "+"float"+" равно "+bottle);
+        System.out.println("Значение переменной "+"pi"+" с типом "+"double"+" равно "+pi);
+
+    }
+}
